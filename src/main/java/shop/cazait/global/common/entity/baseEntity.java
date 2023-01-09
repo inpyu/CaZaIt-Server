bb01fc7f472a687d34356bd619c526600fc80744
ParkRootSeok <g_e_un@naver.com>
2023-01-09 17:03:24 +0900

    베이스 코드

---
+++ src/main/java/shop/cazait/global/common/entity/baseEntity.java
@@ -0,0 +1,16 @@
+package shop.cazait.global.common.entity;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import shop.cazait.global.common.status.BaseStatus;
+
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class baseEntity {
+
+    @Enumerated(EnumType.STRING)
+    private BaseStatus status;
+
+}
